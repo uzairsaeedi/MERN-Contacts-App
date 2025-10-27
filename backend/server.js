07dd3ce1068b400c1f8182d33da366feee18b169
trainee-oss <trainee@devsynth-innovations.com>
2025-10-27 14:54:41 +0500

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ connectDB();
 const app = express();
 
 const port = process.env.PORT || 8000;
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
