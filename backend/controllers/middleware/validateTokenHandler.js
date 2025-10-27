c97a96ee09c08774ce51f11294d1f98bb7e8a21a
trainee-oss <trainee@devsynth-innovations.com>
2025-10-27 12:16:15 +0500

    Testing Automated Pipeline

--- backend/controllers/middleware/validateTokenHandler.js
+++ backend/controllers/middleware/validateTokenHandler.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
     let token;
-    let authHeader = req.headers.Authorization || req.headers.authorization;
+    let authHeader = req.headers.Authorization;
     if (authHeader && authHeader.startsWith("Bearer")) {
         token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
